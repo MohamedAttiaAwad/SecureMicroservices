2518a9ed0ee613a643af5a5f538d5ec7c2b2d660
mohamed attia <devm3aatya@gmail.com>
2022-06-10 18:22:16 +0300

    Add project files.

---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,54 @@
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddInMemoryClients(Config.Clients)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+
+                //.AddInMemoryApiResources(Config.ApiResources)
+                //.AddTestUsers(Config.TestUser())
+                .AddDeveloperSigningCredential();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseIdentityServer();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("hello world");
+                });
+            });
+        }
+    }
+}
---
+++ Movies.API/Data/MovieContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Movies.API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.API.Data
+{
+    public class MovieContextSeed
+    {
+        public static void SeedAsync(MoviesAPIContext moviesAPIContext)
+        {
+            if (!moviesAPIContext.Movie.Any())
+            {
+                var movies = new List<Movie>()
+                {
+                    new Movie(){ Id = 1, Genre = "drama", Title="the film 1", ImageUrl="src", ReleaseDate= new DateTime(1994,12,1),Rating="9.3",Owner="alice" },
+                    new Movie(){ Id = 2, Genre = "drama", Title="the film 2", ImageUrl="src", ReleaseDate= new DateTime(1994,12,1),Rating="9.3",Owner="alice" },
+                    new Movie(){ Id = 3, Genre = "drama", Title="the film 3", ImageUrl="src", ReleaseDate= new DateTime(1994,12,1),Rating="9.3",Owner="alice" },
+                    new Movie(){ Id = 4, Genre = "drama", Title="the film 4", ImageUrl="src", ReleaseDate= new DateTime(1994,12,1),Rating="9.3",Owner="alice" },
+                };
+                moviesAPIContext.Movie.AddRange(movies);
+                moviesAPIContext.SaveChanges();
+            }
+        }
+
+    }
+}
